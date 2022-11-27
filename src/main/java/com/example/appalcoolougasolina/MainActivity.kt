ce404cca841da50db75fef70295b521b93497467
JulianoBaier <julianorenis@gmail.com>
2022-11-27 16:18:18 -0300

    Primeiro app Kotlin e primeira vez usando git

--- src/main/java/com/example/appalcoolougasolina/MainActivity.kt
+++ src/main/java/com/example/appalcoolougasolina/MainActivity.kt
@@ -2,6 +2,8 @@ package com.example.appalcoolougasolina
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -9,4 +11,57 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
+    //função calcular onClick
+    fun calcularPreco(view: View){
+        val precoAlcool =edtPrecoAlcool.text.toString()
+        val precoGasolina =edtPrecoGasolina.text.toString()
+
+        val validaCampos = validarCampos(precoAlcool,precoGasolina)
+
+        if (validaCampos){
+            calcularMelhorPreco(precoAlcool,precoGasolina)
+        }else{
+            txtResultado.text = "Preencha os preços primeiro"
+        }
+
+    }
+
+    private fun calcularMelhorPreco(precoAlcool: String, precoGasolina: String) {
+        //Converter de string para Double
+        val valorAlcool = precoAlcool.toDouble()
+        val valorGasolina = precoGasolina.toDouble()
+
+        // resultado ad formula
+        val resultadoPreco = valorAlcool/valorGasolina
+
+        //laço do resultado da conta
+        if(resultadoPreco>=0.7){
+            txtResultado.text = "Melhor usar Gasolina"
+        }else{
+            txtResultado.text = "Melhor usar Álcool"
+
+        }
+    }
+
+    // Metodo Booleano para verificar se os campos estão vazios"false" ou preenchidos "true"
+    private fun validarCampos(precoAlcool: String, precoGasolina: String):Boolean {
+
+        // varialvel onde armazena  true ou false
+        var camposValidados = true
+
+        //veirificando se campo do preco alcool esta vazio
+        if(precoAlcool==null||precoAlcool.equals("")){
+
+            camposValidados=false
+        }
+
+        //veirificando se campo do preco gasolina esta vazio
+        else if(precoGasolina==null||precoGasolina.equals("")) {
+            camposValidados = false
+        }
+        return camposValidados
+    }
+
+
 }
